d8fd3f4258d989ca24ef6f7cfd85e9389b3b6b85
MiszukUwU <aleksanderowsinski23@gmail.com>
2024-12-08 10:51:38 +0100

    Day 4 done

--- 2024/Day 4/src/Main.java
+++ 2024/Day 4/src/Main.java
@@ -26,60 +26,74 @@ public class Main {
     }
     public static void main(String[] args) throws FileNotFoundException {
         char[][] chars = getAllLines(new File("data.txt"));
+        char[][] tempChars = getAllLines(new File("data.txt"));
         int N = chars.length;
         int M = chars[0].length;
         int sum = 0;
+        int sum2 = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                try {
-                    if (chars[i][j] != 'X') {
-                        continue;
-                    }
+                if (chars[i][j] == 'X') {
                     if (i >= 3) {
-                        if (chars[i+1][j] == 'M' && chars[i+2][j] == 'A' && chars[i+3][j] == 'S') {
+                        if (chars[i - 1][j] == 'M' && chars[i - 2][j] == 'A' && chars[i - 3][j] == 'S') {
                             sum++;
                         }
                         if (j >= 3) {
-                            if (chars[i+1][j-1] == 'M' && chars[i+2][j-2] == 'A' && chars[i+3][j-3] == 'S') {
+                            if (chars[i - 1][j - 1] == 'M' && chars[i - 2][j - 2] == 'A' && chars[i - 3][j - 3] == 'S') {
                                 sum++;
                             }
                         }
-                        if (j <= M-4) {
-                            if (chars[i+1][j+1] == 'M' && chars[i+2][j+2] == 'A' && chars[i+3][j+3] == 'S') {
+                        if (j <= M - 4) {
+                            if (chars[i - 1][j + 1] == 'M' && chars[i - 2][j + 2] == 'A' && chars[i - 3][j + 3] == 'S') {
                                 sum++;
                             }
                         }
                     }
                     if (j >= 3) {
-                        if (chars[i][j-1] == 'M' && chars[i][j-2] == 'A' && chars[i][j-3] == 'S') {
+                        if (chars[i][j - 1] == 'M' && chars[i][j - 2] == 'A' && chars[i][j - 3] == 'S') {
                             sum++;
                         }
                     }
-                    if (j <= M-4) {
-                        if (chars[i][j+1] == 'M' && chars[i][j+2] == 'A' && chars[i][j+3] == 'S') {
+                    if (j <= M - 4) {
+                        if (chars[i][j + 1] == 'M' && chars[i][j + 2] == 'A' && chars[i][j + 3] == 'S') {
                             sum++;
                         }
                     }
-                    if (i <= N-4) {
-                        if (chars[i+1][j] == 'M' && chars[i+2][j] == 'A' && chars[i+3][j] == 'S') {
+                    if (i <= N - 4) {
+                        if (chars[i + 1][j] == 'M' && chars[i + 2][j] == 'A' && chars[i + 3][j] == 'S') {
                             sum++;
                         }
                         if (j >= 3) {
-                            if (chars[i+1][j-1] == 'M' && chars[i+2][j-2] == 'A' && chars[i+3][j-3] == 'S') {
+                            if (chars[i + 1][j - 1] == 'M' && chars[i + 2][j - 2] == 'A' && chars[i + 3][j - 3] == 'S') {
                                 sum++;
                             }
                         }
-                        if (j <= M-4) {
-                            if (chars[i+1][j+1] == 'M' && chars[i+2][j+2] == 'A' && chars[i+3][j+3] == 'S') {
+                        if (j <= M - 4) {
+                            if (chars[i + 1][j + 1] == 'M' && chars[i + 2][j + 2] == 'A' && chars[i + 3][j + 3] == 'S') {
                                 sum++;
                             }
                         }
                     }
-                } catch (Exception e) {
-                    System.out.println(e);
+                }
+                if (chars[i][j] == 'A') {
+                    if (i == 0 || i == N-1 || j == 0 || j == M-1){
+                        continue;
+                    }
+                    if ((chars[i-1][j-1] == 'M' && chars[i+1][j+1] == 'S') || (chars[i-1][j-1] == 'S' && chars[i+1][j+1] == 'M')){
+                        if ((chars[i-1][j+1] == 'M' && chars[i+1][j-1] == 'S') || (chars[i-1][j+1] == 'S' && chars[i+1][j-1] == 'M')){
+                            sum2++;
+                        }
+                    }
                 }
             }
         }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(tempChars[i][j]);
+            }
+            System.out.println();
+        }
         System.out.println(sum);
+        System.out.println(sum2);
     }
 }
