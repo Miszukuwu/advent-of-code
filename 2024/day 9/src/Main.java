41f62320d99b47a4882145c7a76f620052d4ea7d
MiszukUwU <aleksandeowsinski23@gmail.com>
2025-02-19 05:27:17 +0100

    Day 9

--- 2024/day 9/src/Main.java
+++ 2024/day 9/src/Main.java
@@ -8,17 +8,40 @@ public class Main {
         File file = new File("data.txt");
         Scanner sc = new Scanner(file);
         char[] disk = sc.nextLine().toCharArray();
-        Block[] fileBlocks = new Block[disk.length/2];
+        Block[] fileBlocks = new Block[disk.length/2+1];
         for (int i = 0, j = 0; i < disk.length; i+=2,j++) {
-            fileBlocks[j] = new Block(j, (int)disk[i], (i+1<disk.length-1)?(int)disk[i+1]:0);
+            fileBlocks[j] = new Block(j, (int)disk[i]-'0', (i+1<disk.length-1)?(int)disk[i+1]-'0':0);
         }
+        String compactedFIle = "";
         for (Block block : fileBlocks) {
             for (int i = 0; i < block.size; i++) {
-                System.out.println(block.id);
+                System.out.print(block.id);
+                compactedFIle += block.id;
             }
             for (int i = 0; i < block.freeSpace; i++) {
-                System.out.println(".");
+                System.out.print(".");
+                compactedFIle += ".";
             }
         }
+        System.out.println();
+        for (int i = compactedFIle.length()-1; i >= 0; i--) {
+            int length = compactedFIle.length();
+            int spaceIndex = compactedFIle.indexOf('.');
+            if (spaceIndex>i)
+                break;
+            compactedFIle = compactedFIle.substring(0, spaceIndex) + compactedFIle.charAt(i) + compactedFIle.substring(spaceIndex+1, i);
+            for (int j = 0; j <  length - i; j++) {
+                compactedFIle += '.';
+            }
+//            System.out.println(compactedFIle);
+        }
+        int sum = 0;
+        for (int i = 0; i < compactedFIle.length(); i++) {
+            if (compactedFIle.charAt(i)=='.'){
+                break;
+            }
+            sum += (compactedFIle.charAt(i)-'0')*i;
+        }
+        System.out.println(sum);
     }
 }
